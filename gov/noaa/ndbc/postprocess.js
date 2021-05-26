0f597336e67b6be684066d8c5598f6b89f19535d
Russ Savage <russ@influxdata.com>
2021-05-25 19:46:31 -0700

    feat: adding active stations

---
+++ gov/noaa/ndbc/postprocess.js
@@ -0,0 +1,55 @@
+// This can be a typescript file as well
+
+// Helper library written for useful postprocessing tasks with Flat Data
+// Has helper functions for manipulating csv, txt, json, excel, zip, and image files
+import { readTXT, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+
+// Step 1: Read the downloaded_filename JSON
+const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
+const csv = await readTXT(filename)
+//console.log(csv)
+
+const re = /\s+/
+const latest_columns = "wind_dir_degt,wind_speed_mps,gust_speed_mps,significant_wave_height_m,dominant_wave_period_sec,avg_wave_period_sec,wave_dir_degt,sea_level_pressure_hpa,air_temp_degc,sea_surface_temp_degc,dewpoint_temp_degc,station_visibility_nmi,pressure_tendency_hpa,water_level_ft".split(",")
+const missing_data_list = "MM,999,9999.0,999.0,99.0,99.00".split(",")
+
+const jsonArray = csv.split('\n').map(line => {
+    if (!line.startsWith("#") && line.length != 0) {
+        const readings = line.split(re)
+        let ret = {
+            station_id: readings.shift(),
+            lat: parseFloat(readings.shift()),
+            lon: parseFloat(readings.shift()),
+        }
+        let year = readings.shift()
+        let month = readings.shift()
+        let day = readings.shift()
+        let hour = readings.shift()
+        let minute = readings.shift()
+        let date = year + "-" + month + "-" + day + "T" + hour + ":" + minute + "+0700" //2006-01-02T15:04
+
+        ret["timestamp"] = new Date(date).valueOf() + "000000"
+
+        readings.forEach((value, index) => {
+            if (!missing_data_list.includes(value)) {
+                ret[latest_columns[index]] = parseFloat(value)
+            }
+        })
+        return ret
+    }
+})
+
+// Step 2: Filter specific data we want to keep and write to a new JSON file
+// const currencyRates = Object.values(json.bpi); // convert property values into an array
+// const filteredCurrencyRates = currencyRates.map(rate => ({ 
+//     currency: rate.description,
+//     bitcoinRate: rate.rate
+// }));
+
+// Step 3. Write a new JSON file with our filtered data
+// const newFilename = `btc-price-postprocessed.json` // name of a new file to be saved
+// await writeJSON(newFilename, filteredCurrencyRates) // create a new JSON file with just the Bitcoin price
+// console.log("Wrote a post process file")
+
+// Optionally delete the original file
+// await removeFile('./btc-price.json') // equivalent to removeFile('btc-price.json')
